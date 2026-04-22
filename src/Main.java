c12fd0c943d503588bdc854f1f96f20469379a8c
Walacy Pereira <143743509+WollygrinX@users.noreply.github.com>
2026-04-22 14:49:18 -0300

    Refatoração: substituindo código padrão pelo Habit Tracker modularizado

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,94 @@
-//TIP Para <b>executar</b> o código, pressione <shortcut actionId="Run"/> ou
-// clique no ícone <icon src="AllIcons.Actions.Execute"/> no gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Pressione <shortcut actionId="ShowIntentionActions"/> com seu caret no texto destacado
-        // para ver como IntelliJ IDEA sugere corrigi-lo.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        menu(sc);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Pressione <shortcut actionId="Debug"/> para iniciar a depuração do seu código. Definimos um ponto de interrupção <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-            // para você, mas você sempre pode adicionar mais pressionando <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+    public static void menu(Scanner sc){
+        int meta = 0;
+        int streak = 0;
+        int op = 0;
+
+        while (op != 5){
+            System.out.println("### Controle de Hábitos (Habit Tracker) ###");
+            System.out.println("1 - Registrar conclusão de hábito hoje (ex:Estudei Java)");
+            System.out.println("2 - Ver sequência de dias (streak)");
+            System.out.println("3 - Redefinir meta semanal.");
+            System.out.println("4 - Registrar sentimentos.");
+            System.out.println("5 - Sair");
+
+            op = sc.nextInt();
+            sc.nextLine();
+
+            switch (op){
+                case 1:
+                    streak++;
+                    registrar(sc);
+                    break;
+
+                 case 2:
+                     sequencia(streak);
+                     break;
+
+                 case 3:
+                     meta = metasemanal(sc, meta);
+                     break;
+
+                 case 4:
+                    registrarSentimento(sc);
+                    break;
+                case 5:
+                    System.out.println("Saindo, até mais!");
+                    System.out.println("\n--------------------------\n");
+                    break;
+
+                 default:
+                    System.out.println("ATENÇÂO: opção invalida!");
+
+            }
+        }
+        sc.close();
+    }
+
+    public static String registrar(Scanner sc){
+        System.out.println("Qual hábito você quer registrar? ");
+        String habito = sc.nextLine();
+        System.out.println("Mostrando hábito registrado: " +  habito);
+        System.out.println("\n--------------------------\n");
+        return habito;
+    }
+
+    public static void sequencia(int streak){
+        System.out.println("Mostra sequência de " + streak + " dias.");
+        System.out.println("\n--------------------------\n");
+    }
+
+    public static int metasemanal(Scanner sc, int meta){
+        System.out.println("Sua meta atual é " + meta);
+        System.out.println("Digite a nova meta semanal: ");
+        meta = sc.nextInt();
+        sc.nextLine();
+        while(meta > 7 || meta < 1){
+            System.out.println("A semana só tem 7 dias");
+            System.out.println("Digite novamente: ");
+            meta = sc.nextInt();
+            sc.nextLine();
+        }
+        System.out.println("Você digitou " + meta + " dias");
+        System.out.println("Meta atualizada com sucesso!");
+        System.out.println("\n--------------------------\n");
+        return meta;
+    }
+   public static void registrarSentimento(Scanner sc){
+       System.out.println("Olá, como você se chama?: ");
+       String nome =  sc.nextLine();
+       System.out.println("O que você está sentindo? ");
+       String sentimento = sc.nextLine();
+       System.out.println("Entendo... você está se sentindo " + sentimento);
+       System.out.println("Lembre-se: Mesmo nos dias difíceis, você não desistiu do seu código, " + nome + " Nunca esqueça disso!!");
+       System.out.println("\n--------------------------\n");
+   }
 }
+
+
